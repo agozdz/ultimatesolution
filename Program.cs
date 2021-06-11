afe323f696fc331bd07dfeaa64ad173d5344aa78
agozdz <aleksandra.gozdz@currenda.pl>
2021-06-11 08:49:48 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyUltimateSolution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            Console.WriteLine("Hello World!");
+            for(int i = 0; i < 10; i++) {
+                System.Threading.Thread.Sleep(1000);
+                Console.WriteLine(random.Next(1000, 9999));
+            }
+        }
+    }
+}
